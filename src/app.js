12c5b2afd44f2f044f02502cb90fde180ece2f7f
cfiander <cfiander90@gmail.com>
2019-08-05 01:01:36 -0700

    asdf

---
+++ src/app.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+const morgan = require('morgan');
+
+
+
+const app = express();
+
+const morganOption = (process.env.NODE_ENV === 'production') // For server side logs
+  ? 'tiny'
+  : 'common';
+app.use(cors({
+    origin: CLIENT_ORIGIN
+  }));
+app.use(helmet());
+app.use(morgan(morganOption));
+app.use(express.json());
+
+
+app.get(`/`, (req, res) => {
+  res.send("Hello, world!")
+})
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+// Add NODE_ENV check to prevent stacktrace leak
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: app.get('env') === 'development' ? err : {}
+    });
+  });
+
+  module.exports = app;
